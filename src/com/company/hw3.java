271ad38a523215f3ec4628dfc5ad6a8d75324244
emilyyu <yuhuayu.emily@gmail.com>
2022-09-19 22:40:48 +0300

    rest of homework

--- src/com/company/hw3.java
+++ src/com/company/hw3.java
@@ -1,7 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class hw3 {
+
     public static void main(String[] args) {
-        // nothing here yet
+        ArrayList<String> a = split (args);
+        for (String b : a)
+            System.out.print (b + " ");
+    }
+
+    public static ArrayList<String> split (String[] arr) {
+        String arg1 = arr[0];
+        int i = arg1.indexOf (arr[1]);
+        int j = 0;
+        ArrayList<String> result = new ArrayList<String>();
+        if (i == -1) {
+            result.add (arg1);
+            return result;
+        }
+
+        if (i != 0) {
+            result.add (arg1.substring (0, i));
+        }
+
+        while (j != -1) {
+            arg1 = arg1.substring (i + arr[1].length(), arg1.length());
+            j = arg1.indexOf (arr[1]);
+            if (j == -1) {
+                result.add (arg1);
+            }
+            else {
+                if (!arg1.substring (0, j).equals ("")) {
+                    result.add (arg1.substring(0, j));
+                }
+                i = j;
+            }
+        }
+        return result;
     }
 }
